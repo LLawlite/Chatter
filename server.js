2d7fd0edd23f932c1f069a4f7f04db87d7272455
Vaibhav kale <vaibhavkale2201@gmaile.com>
2023-01-23 18:45:55 +0530

    update oringn url

--- server.js
+++ server.js
@@ -59,7 +59,8 @@ const server = app.listen(
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
   cors: {
-    origin: 'http://localhost:3000',
+    // origin: 'http://localhost:3000',
+    origin: 'https://lucent-cascaron-b0f8ee.netlify.app/',
     // origin: 'https://chatter-4f5d2.web.app',
     // credentials: true,
   },
